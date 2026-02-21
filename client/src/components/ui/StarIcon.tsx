e8aff480de46a3b59efe8de4996b04a97750910b
Pranit Chiman <pranitchiman@gmail.com>
2026-02-22 00:26:12 +0530

    frontend refactor

---
+++ client/src/components/ui/StarIcon.tsx
@@ -0,0 +1,13 @@
+export function StarIcon({ size = 14 }: { size?: number }) {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      aria-hidden="true"
+    >
+      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 21 12 17.77 5.82 21 7 14.14 2 9.27l6.91-1.01L12 2z" />
+    </svg>
+  );
+}
